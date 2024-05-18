3696ce3f590589c540bdd0462494889463b67e5a
VenketeshPratap <venketeshmall2@gmail.com>
2024-05-18 14:16:12 +0530

    Your commit message here

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-# main.py
+
+import re
+import pandas as pd
+import pyttsx3
+from sklearn import preprocessing
+from sklearn.tree import DecisionTreeClassifier,_tree
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.svm import SVC
+import csv
+import warnings
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
 
 from flask import Flask, render_template, request, redirect, url_for, session, g, jsonify
 import sqlite3
 import re
 import hashlib
-# from hchatbot.chat_bot import run_chatbot
 import os
 from dotenv import load_dotenv
 import json
@@ -26,6 +40,277 @@ app = Flask(__name__)
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 DATABASE = 'healthdb.db'
+
+training = pd.read_csv('Data/Training.csv')
+testing= pd.read_csv('Data/Testing.csv')
+cols= training.columns
+cols= cols[:-1]
+x = training[cols]
+y = training['prognosis']
+y1= y
+result_list = []
+usersymptom='fever'
+modelresult=''
+reduced_data = training.groupby(training['prognosis']).max()
+
+#mapping strings to numbers
+le = preprocessing.LabelEncoder()
+le.fit(y)
+y = le.transform(y)
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+testx    = testing[cols]
+testy    = testing['prognosis']
+testy    = le.transform(testy)
+
+
+clf1  = DecisionTreeClassifier()
+clf = clf1.fit(x_train,y_train)
+# print(clf.score(x_train,y_train))
+# print ("cross result========")
+scores = cross_val_score(clf, x_test, y_test, cv=3)
+# print (scores)
+print (scores.mean())
+
+
+model=SVC()
+model.fit(x_train,y_train)
+print("for svm: ")
+print(model.score(x_test,y_test))
+
+importances = clf.feature_importances_
+indices = np.argsort(importances)[::-1]
+features = cols
+
+def readn(nstr):
+    engine = pyttsx3.init()
+
+    engine.setProperty('voice', "english+f5")
+    engine.setProperty('rate', 130)
+
+    engine.say(nstr)
+    engine.runAndWait()
+    engine.stop()
+
+
+severityDictionary=dict()
+description_list = dict()
+precautionDictionary=dict()
+
+symptoms_dict = {}
+
+for index, symptom in enumerate(x):
+       symptoms_dict[symptom] = index
+def calc_condition(exp,days):
+    sum=0
+    for item in exp:
+         sum=sum+severityDictionary[item]
+    if((sum*days)/(len(exp)+1)>13):
+        result_list.append( "You should take the consultation from doctor. \n")
+    else:
+        result_list.append(" It might not be that bad but you should take precautions.\n ")
+        print("It might not be that bad but you should take precautions.")
+
+
+def getDescription():
+    global description_list
+    with open('MasterData/symptom_Description.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            _description={row[0]:row[1]}
+            description_list.update(_description)
+
+
+
+
+def getSeverityDict():
+    global severityDictionary
+    with open('MasterData/symptom_severity.csv') as csv_file:
+
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        try:
+            for row in csv_reader:
+                _diction={row[0]:int(row[1])}
+                severityDictionary.update(_diction)
+        except:
+            pass
+
+
+def getprecautionDict():
+    global precautionDictionary
+    with open('MasterData/symptom_precaution.csv') as csv_file:
+
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            _prec={row[0]:[row[1],row[2],row[3],row[4]]}
+            precautionDictionary.update(_prec)
+
+
+def getInfo():
+    print("-----------------------------------HealthCare ChatBot-----------------------------------")
+    result_list.append("Health Status-- \t\n")
+    print("\nYour Name? \t\t\t\t",end="->")
+    name='Patient X'
+    result_list.append("Hello, Here is points to follow \t\n")
+    print("Hello, ",name)
+
+def check_pattern(dis_list,inp):
+    pred_list=[]
+    inp=inp.replace(' ','_')
+    patt = f"{inp}"
+    regexp = re.compile(patt)
+    pred_list=[item for item in dis_list if regexp.search(item)]
+    if(len(pred_list)>0):
+        return 1,pred_list
+    else:
+        return 0,[]
+def sec_predict(symptoms_exp):
+    df = pd.read_csv('Data/Training.csv')
+    X = df.iloc[:, :-1]
+    y = df['prognosis']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=20)
+    rf_clf = DecisionTreeClassifier()
+    rf_clf.fit(X_train, y_train)
+
+    symptoms_dict = {symptom: index for index, symptom in enumerate(X)}
+    input_vector = np.zeros(len(symptoms_dict))
+    for item in symptoms_exp:
+      input_vector[[symptoms_dict[item]]] = 1
+
+    return rf_clf.predict([input_vector])
+
+
+def print_disease(node):
+    node = node[0]
+    val  = node.nonzero()
+    disease = le.inverse_transform(val[0])
+    return list(map(lambda x:x.strip(),list(disease)))
+
+def tree_to_code(tree, feature_names):
+    tree_ = tree.tree_
+    feature_name = [
+        feature_names[i] if i != _tree.TREE_UNDEFINED else "undefined!"
+        for i in tree_.feature
+    ]
+
+    chk_dis=",".join(feature_names).split(",")
+    symptoms_present = []
+
+    while True:
+
+        print("\nEnter the symptom you are experiencing  \t\t",end="->")
+        disease_input = usersymptom
+        conf,cnf_dis=check_pattern(chk_dis,disease_input)
+        if conf==1:
+            print("searches related to input: ")
+            for num,it in enumerate(cnf_dis):
+                print(num,")",it)
+                modelresult=str(num)+")"+str(it)
+                result_list.append(modelresult)
+                modelresult=''
+            if num!=0:
+                print(f"Select the one you meant (0 - {num}):  ", end="")
+                conf_inp = 1
+            else:
+                conf_inp=0
+
+            disease_input=cnf_dis[conf_inp]
+            break
+            # print("Did you mean: ",cnf_dis,"?(yes/no) :",end="")
+            # conf_inp = input("")
+            # if(conf_inp=="yes"):
+            #     break
+        else:
+            print("Enter valid symptom.")
+
+    while True:
+        try:
+            num_days=4
+            break
+        except:
+            print("Enter valid input.")
+    def recurse(node, depth):
+        indent = "  " * depth
+        if tree_.feature[node] != _tree.TREE_UNDEFINED:
+            name = feature_name[node]
+            threshold = tree_.threshold[node]
+
+            if name == disease_input:
+                val = 1
+            else:
+                val = 0
+            if  val <= threshold:
+                recurse(tree_.children_left[node], depth + 1)
+            else:
+                symptoms_present.append(name)
+                recurse(tree_.children_right[node], depth + 1)
+        else:
+            present_disease = print_disease(tree_.value[node])
+            # print( "You may have " +  present_disease )
+            red_cols = reduced_data.columns
+            symptoms_given = red_cols[reduced_data.loc[present_disease].values[0].nonzero()]
+            # dis_list=list(symptoms_present)
+            # if len(dis_list)!=0:
+            #     print("symptoms present  " + str(list(symptoms_present)))
+            # print("symptoms given "  +  str(list(symptoms_given)) )
+            print("Are you experiencing any ")
+            symptoms_exp=[]
+            for syms in list(symptoms_given):
+                inp=""
+                print(syms,"? : ",end='')
+                while True:
+                    inp='no'
+                    if(inp=="yes" or inp=="no"):
+                        break
+                    else:
+                        print("provide proper answers i.e. (yes/no) : ",end="")
+                if(inp=="yes"):
+                    symptoms_exp.append(syms)
+
+            second_prediction=sec_predict(symptoms_exp)
+            # print(second_prediction)
+            calc_condition(symptoms_exp,num_days)
+            if(present_disease[0]==second_prediction[0]):
+                print("You may have ", present_disease[0])
+                modelresult="You may have "+present_disease[0]
+                result_list.append(modelresult)
+                print(description_list[present_disease[0]])
+                result_list.append("\n")
+                result_list.append(description_list[present_disease[0]])
+
+                # readn(f"You may have {present_disease[0]}")
+                # readn(f"{description_list[present_disease[0]]}")
+
+            else:
+                print("You may have ", present_disease[0], "or ", second_prediction[0])
+                print(description_list[present_disease[0]])
+                print(description_list[second_prediction[0]])
+                result_list.append(description_list[present_disease[0]]+"\n"+description_list[second_prediction[0]])
+
+            # print(description_list[present_disease[0]])
+            precution_list=precautionDictionary[present_disease[0]]
+            print("Take following measures : ")
+            result_list.append("\n")
+            result_list.append("Take following measures : \n")
+            for  i,j in enumerate(precution_list):
+                print(i+1,")",j, ' ****************')
+                printsave=str(i+1) + ")" +str(j)
+                result_list.append(printsave+"\n")
+
+
+            # confidence_level = (1.0*len(symptoms_present))/len(symptoms_given)
+            # print("confidence level is " + str(confidence_level))
+
+    recurse(0, 1)
+
+
+
+
+
 
 
 def generate_summary(symtoms):
@@ -130,30 +415,25 @@ def register():
 def index():
     if 'loggedin' not in session:
         return redirect(url_for('login'))
-
-    if request.method == 'POST':
-        symptom_input = request.form['symptom']
-        try:
-            days_input = int(request.form['days'])
-        except ValueError:
-            return jsonify({"error": "Please enter a valid number for days."})
-
-        result = 'Chatbot response based on symptom input.'
-        return jsonify(result)
-
     return render_template('index.html', username=session['username'])
 
-@app.route('/submit', methods=['POST'])
+
+@app.route('/submit', methods=['GET','POST'])
 def submit():
     symptoms = request.form.get('symptoms')
     severity = request.form.get('severity')
     feeling = request.form.get('feeling')
-
-    # Here you would process the data and generate the result
-    result=generate_summary(symptoms+severity+feeling)
-    result = result+"This is a sample result based on your input."
-
-    return jsonify({'result': result})
+    usersymptom=symptoms
+    # Process the data and generate the result
+    getSeverityDict()
+    getDescription()
+    getprecautionDict()
+    getInfo()
+    tree_to_code(clf, cols)
+    result =''.join(result_list)
+    print(result,'23333)))))******************@@@@@@@@@@@@@@@')
+    # Render the template with the result
+    return render_template('index.html', result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
